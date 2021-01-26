54a754ca8cd12c06edf562f7b348e67f7b697063
remzi chaushev <remzitdf@abv.bg>
2021-01-26 18:09:44 +0200

    Add project files.

---
+++ GamingStore/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace GamingStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = double.Parse(Console.ReadLine());
+
+            double currentBalance = balance;
+            double moneySpend = 0;
+
+            while (true)
+            {
+                string game = Console.ReadLine();
+
+                if (game == "Game Time")
+                {
+                    break;
+                }
+                double price = 0;
+                switch (game)
+                {
+                    case "OutFall 4": price = 39.99; break;
+                    case "CS: OG": price = 15.99; break;
+                    case "Zplinter Zell": price = 19.99; break;
+                    case "Honored 2": price = 59.99; break;
+                    case "RoverWatch": price = 29.99; break;
+                    case "RoverWatch Origins Edition": price = 39.99; break;
+                    default:
+                        Console.WriteLine("Not Found");
+                        continue;
+                }
+                if (currentBalance < price)
+                {
+                    Console.WriteLine("Too Expensive");
+                    continue;
+                }
+
+                moneySpend += price;
+                currentBalance -= price;
+                Console.WriteLine($"Bought {game}");
+
+                if (currentBalance == 0)
+                {
+                    Console.WriteLine("Out of money!");
+                    return;
+                }
+
+            }
+            Console.WriteLine($"Total spent: ${moneySpend:F2}. Remaining: ${balance - moneySpend:F2}");
+
+        }
+    }
+}
---
+++ SortNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int highest =int.MinValue;
+            int middle = int.MinValue;
+            int lowest = int.MinValue;
+
+            for (int i = 0; i < 3; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n > highest)
+                {
+                    lowest = middle;
+                    middle = highest;
+                    highest = n;
+                }
+                else if (n > middle)
+                {
+                    lowest = middle;
+                    middle = n;
+                }
+                else if(n>lowest)
+                {
+                    lowest = n;
+                }
+            }
+            Console.WriteLine(highest);
+            Console.WriteLine(middle);
+            Console.WriteLine(lowest);
+        }
+    }
+}
